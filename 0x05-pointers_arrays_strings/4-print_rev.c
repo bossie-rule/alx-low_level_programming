0d1114b1e193c8c0e8e7731d6427b4850dbe3f2e
bossie-rule <cintaboss@gmail.com>
2022-09-18 23:45:54 -0700

    function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,16 +3,21 @@
 /**
  * print_rev - prints a string in reverse
  * @s: prints a string
+ *
+ * Return: nothing
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i;
 
-	while (s[index++])
-		len++;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
-}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
