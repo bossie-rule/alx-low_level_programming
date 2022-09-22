c6149388fae1d219de8c089ea4a0fe68dd5d2c0d
bossie-rule <cintaboss@gmail.com>
2022-09-22 02:21:01 -0700

    Add one line to this code, so that the program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,13 +11,13 @@ int main(void)
 	/*
 	 * write code of line here...
 	 * Remember:
-	 * - not allowed to use a
-	 * - not allowed to modify p
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...this prints 98\n */
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
