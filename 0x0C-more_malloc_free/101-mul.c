6e6bf809ba2015be200d0876190884aeecdb2efb
bossie-rule <cintaboss@gmail.com>
2022-10-07 06:00:41 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -221,7 +221,6 @@ int main(int argc, char *argv[])
 		get_prod(next_prod, argv[1], digit, zeroes++);
 		add_nums(final_prod, next_prod, size - 1);
 	}
-
 	for (index = 0; final_prod[index]; index++)
 	{
 		if (final_prod[index] != 'x')
